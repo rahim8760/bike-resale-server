dc02c1cdec9ba067b0630d5683273e3f98e5fe2f
md abdur rahim <rahim8760>
2022-11-26 02:45:43 +0600

    get product by categoty

--- index.js
+++ index.js
@@ -13,7 +13,52 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.scxie4k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const db=client.db('Bike_Resale')
+        const UserCollection=db.collection('Users');
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result= await UserCollection.insertOne(user)
+            res.send(result); 
+        })
+        const CategoryCollection=db.collection('Category');
+        app.post('/category', async(req, res) => {
+            const category = req.body;
+            const result= await CategoryCollection.insertOne(category)
+            res.send(result); 
+        })
+        app.get('/category', async(req, res)=>{
+            const serQuery={}
+            const categoryCursor=CategoryCollection.find(serQuery)
+            const category=await categoryCursor.toArray()
+            res.send(category)
+        })
+        const ProductCollection=db.collection('Products');
+        app.post('/product', async(req, res) => {
+            const product = req.body;
+            const result= await ProductCollection.insertOne(product)
+            res.send(result); 
+        })
+        app.get('/product', async(req, res)=>{
+            const serQuery={}
+            const productCursor=ProductCollection.find(serQuery)
+            const product=await productCursor.toArray()
+            res.send(product)
+        })
+        app.get('/product/:id', async(req, res)=>{
+            const id =req.params.id;
+            const serQuery={category:id}
+            const productCursor=ProductCollection.find(serQuery)
+            const product=await productCursor.toArray()
+            res.send(product)
+        })
+        
+    }finally{
 
+    }
+}
+run().catch(error => console.log(error));
 
 
 app.get('/', (req, res) => {
